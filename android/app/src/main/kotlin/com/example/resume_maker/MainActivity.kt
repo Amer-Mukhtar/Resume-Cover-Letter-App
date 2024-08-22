5d1203b9d4bd740e673968482b220570bc2a9ea3
Amer Mukhtar <amermukhtar19@gmail.com>
2024-08-22 12:41:54 +0500

    update

--- android/app/src/main/kotlin/com/example/resume_maker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.resume_maker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/Utility/constants/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Colors
-{
-Color black=  CupertinoColors.black;
-}
